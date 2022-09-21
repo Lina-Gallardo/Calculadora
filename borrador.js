e445073165357535b2dc02e60e8ea00a21392e48
Lina Gallardo <lgallard3@cuc.edu.co>
2022-09-21 18:13:40 -0500

    Error

--- borrador.js
+++
@@ -1,8 +0,0 @@
-function num (number){
-  if(document.getElementById("celda").value==""){
-    document.getElementById('celda').value = number;
-  }else{
-    anterior = num(number);
-  }
-  return number;
-}
